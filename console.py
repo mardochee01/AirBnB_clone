d8b9326ecb0d7ed29ec32e81a67d0e3dfe8dbf31
mardochee01 <mardoche.gneran@gmail.com>
2022-08-05 22:34:03 +0000

    update

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines the HBnB console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program"""
+        print("")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
