5375f63f788b2bfa42900a4be6689f418fd3ef76
mardochee01 <mardoche.gneran@gmail.com>
2022-08-05 18:08:03 +0000

    update

---
+++ models/engine/file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that serializes and deserializes JSON file
+"""
+
+class FileStorage:
+    """
+    Class that serializes instances to a JSON file
+    and deserializes JSON file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(obj.__class.__name, obj.id)
+        self.__object[key] = obj
