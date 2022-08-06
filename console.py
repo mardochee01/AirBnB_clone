fb02abcb16c33d90bd8356f259ab7ab0cfbdc8ed
mardochee01 <mardoche.gneran@gmail.com>
2022-08-06 11:02:57 +0000

    update consol.py

--- console.py
+++ console.py
@@ -8,6 +8,10 @@ class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter"""
     prompt = "(hbnb) "
 
+    def emptyline(self):
+        """add empty line"""
+        pass
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
